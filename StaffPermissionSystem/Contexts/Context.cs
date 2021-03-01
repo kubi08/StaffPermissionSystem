ca0a817862778998b7d6f8eaa7a33afaf616e334
Kubilay <kubilay@haydesoft.com>
2021-03-01 22:50:33 +0300

    Try Commit

--- StaffPermissionSystem/Contexts/Context.cs
+++ StaffPermissionSystem/Contexts/Context.cs
@@ -13,9 +13,23 @@ namespace StaffPermissionSystem.Contexts
         {
             optionsBuilder.UseSqlServer("");
         }
-        
 
-       public DbSet<ProductCategory> ProductCategories { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasMany(I => I.ProductCategories)
+                .WithOne(I => I.Product).HasForeignKey(I => I.ProductId);
+
+            modelBuilder.Entity<Category>().HasMany(I => I.ProductCategories)
+               .WithOne(I => I.Category).HasForeignKey(I => I.ProductId);
+            modelBuilder.Entity<ProductCategory>().HasIndex(I => new
+            {
+                I.CategoryId,
+                I.ProductId
+
+            }).IsUnique();
+        }
+
+        public DbSet<ProductCategory> ProductCategories { get; set; }
        public DbSet<Category> Categories { get; set; }
        public DbSet<Product> Products { get; set; }
 
